d2a6b962b2b7f56efbe1d90c48de917ded507175
stephenrey23 <stephenjreyes41@gmail.com>
2026-05-15 01:01:56 -0600

    greyhound.cy.js

--- cypress/e2e/greyhound.cy.js
+++ cypress/e2e/greyhound.cy.js
@@ -11,10 +11,16 @@ describe('Greyhound project', () => {
             body: { status: 'success' }
         }).as('mockPayment');
 
-        cy.visit('/', {
+        cy.visit('https://www.greyhound.com', {
         headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
-        }
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.9',
+        },
+        onBeforeLoad(win) {
+        Object.defineProperty(win.navigator, 'webdriver', {
+         get: () => false,
+        });
+        },
         });
         cy.handlePrivacyBanner();
 
