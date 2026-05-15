a404282ce10e950fbce22dea70c898cc6c89a69d
stephenrey23 <stephenjreyes41@gmail.com>
2026-05-15 00:13:27 -0600

    cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    baseUrl: 'https://www.greyhound.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
+    viewportWidth: 1280,
+    viewportHeight: 720
   },
 });
