5fc16bd548adef276358118f47348f46e2e19940
stephenrey23 <stephenjreyes41@gmail.com>
2026-05-15 00:20:41 -0600

    commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,13 @@
 Cypress.Commands.add('handlePrivacyBanner', () => {
-    cy.log('### COMMAND: Handling Cookies ###');
-    cy.get('[data-testid="uc-ccpa-button"]', { timeout: 15000 })
-      .should('be.visible')
-      .click();
+  cy.log('### COMMAND: Checking for Cookies Banner ###');
+  cy.get('body').then(($body) => {
+    if ($body.find('[data-testid="uc-ccpa-button"]').length > 0) {
+      cy.log('### Banner found, clicking... ###');
+      cy.get('[data-testid="uc-ccpa-button"]').click();
+    } else {
+      cy.log('### Banner not found, skipping... ###');
+    }
+  });
 });
 
 Cypress.Commands.add('clickWithoutNewTab', (selector) => {
